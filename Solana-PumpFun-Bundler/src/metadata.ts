a9f5fe618b30d7128258b84b8296a95190955c14
yqq <youngqqcn@gmail.com>
2024-11-28 15:59:06 +0800

    测试pump.fun创建+买入+卖出成功

--- Solana-PumpFun-Bundler/src/metadata.ts
+++ Solana-PumpFun-Bundler/src/metadata.ts
@@ -1,13 +1,13 @@
 const metadata = {
-    "name": "Bolt token",
-    "symbol": "Bolt2",
-    "description": "Brave Veer & Bolt",
+    "name": "Yuzuki Choco",
+    "symbol": "CHOCO",
+    "description": "Good evening! My cute students. Wait a sec!The school nurse at Makai School. She is very popular with both boys and girls at school, and is constantly receiving requests to see the boys. She loves sweets, and is scolded when she is seen leaving candy bags scattered on her desk.",
     "image": "",
     "showName": true,
     "createdOn": "https://pump.fun",
-    "twitter": "https://x.com/pepa_inu",
-    "telegram": "https://t.me/pepaonsols",
-    "website": "https://www.pepa-inu.com"
+    "twitter": "https://twitter.com/ookamimio",
+    "telegram": "",
+    "website": ""
 }
 
 export default metadata;
