8a0374c9661403353a56931e1906975c8a089ba4
yqq <youngqqcn@gmail.com>
2025-01-23 19:19:21 +0800

    update

--- src/trade_on_raydium/swap.ts
+++ src/trade_on_raydium/swap.ts
@@ -60,12 +60,12 @@ export async function swap(
     }
 
     // TODO: 是否有更好的方式？
-    let unitLimit = 8_0000;
+    let unitLimit = 8_3000;
     if (input.buyToken == "base") {
         // 买入
-        unitLimit = 8_0000;
+        unitLimit = 8_3000;
     } else {
-        unitLimit = 6_0000;
+        unitLimit = 6_3000;
     }
     // console.log("swap: ", input);
 
